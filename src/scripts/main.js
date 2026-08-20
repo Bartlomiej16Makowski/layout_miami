c6f33f52db3ceb8fa4bf4379ce2465e9ec5ffcb5
BartekMak <bartlomiej.m.makowski@gmail.com>
2026-08-21 00:59:42 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 const form = document.querySelector('#myform');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-});
+if (form) {
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
+}
+
+function toggleMenuScroll() {
+
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--has-open-menu');
+  } else {
+    document.body.classList.remove('page--has-open-menu');
+  }
+}
+
+toggleMenuScroll();
+
+window.addEventListener('hashchange', toggleMenuScroll);
